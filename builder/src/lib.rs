432745cbd5e7400656dac5942aa93692282696e1
gftea <ckq614@gmail.com>
2023-04-08 19:13:44 +0200

    improve codes

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,88 +8,86 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let name = &input.ident;
 
-    let (fields, types): (Vec<_>, Vec<_>) = match input.data {
+    // extract  all the fields
+    let (fields, types, is_optionals) = match input.data {
         syn::Data::Struct(ref data) => {
-            let fields = data.fields.iter().filter_map(|field| {
+            let fields = data.fields.iter().map(|field| {
                 let field_name = field.ident.as_ref().unwrap();
                 let field_type = &field.ty;
 
-                let is_optional = match field_type {
-                    syn::Type::Path(p) => p.path.segments.iter().any(|s| s.ident == "Option"),
-                    _ => todo!(),
-                };
-                if is_optional {
-                    return None;
-                }
-                Some((
-                    quote_spanned! {field_name.span()=>
-                        #field_name
-                    },
-                    quote_spanned! {field_type.span()=>
-                        #field_type
-                    },
-                ))
-            });
-            fields.unzip()
-        }
-        syn::Data::Enum(_) => todo!(),
-        syn::Data::Union(_) => todo!(),
-    };
-    let (optional_fields, optional_types): (Vec<_>, Vec<_>) = match input.data {
-        syn::Data::Struct(ref data) => {
-            let fields = data.fields.iter().filter_map(|field| {
-                let field_name = field.ident.as_ref().unwrap();
-                let field_type = &field.ty;
-                let is_optional = match field_type {
-                    syn::Type::Path(p) => p.path.segments.iter().any(|s| s.ident == "Option"),
-                    _ => todo!(),
-                };
-                if !is_optional {
-                    return None;
-                }
-                let type_in_option = match field_type {
+                // check if the field is optional
+                let (field_type, is_optional) = match field_type {
                     syn::Type::Path(p) => {
-                        let s = p
-                            .path
-                            .segments
-                            .iter()
-                            .find(|s| s.ident == "Option")
-                            .unwrap();
+                        let s = p.path.segments.iter().next().unwrap();
 
-                        match &s.arguments {
-                            syn::PathArguments::AngleBracketed(a) => {
-                                match a.args.iter().next().unwrap() {
-                                    syn::GenericArgument::Type(t) => t,
-                                    _ => todo!(),
+                        if s.ident == "Option" {
+                            match &s.arguments {
+                                syn::PathArguments::AngleBracketed(a) => {
+                                    match a.args.iter().next().unwrap() {
+                                        syn::GenericArgument::Type(t) => (t, true),
+                                        _ => todo!(),
+                                    }
                                 }
+                                _ => todo!(),
                             }
-                            _ => todo!(),
+                        } else {
+                            (field_type, false)
                         }
                     }
                     _ => todo!(),
                 };
 
-                Some((
-                    quote_spanned! {field_name.span()=>
+                // each field is deconstructed into a tuple of (field_name, (field_type, is_optional))
+                (
+                    quote_spanned!(field_name.span()=>
                         #field_name
-                    },
-                    quote_spanned! {field_type.span()=>
-                        #type_in_option
-                    },
-                ))
+                    ),
+                    (
+                        quote_spanned!(field_type.span()=>
+                            #field_type
+                        ),
+                        is_optional,
+                    ),
+                )
             });
-            fields.unzip()
+            // recruisively deconstruct the fields into a tuple of (field_names, field_types, is_optionals)
+            let (field_names, type_info): (Vec<_>, Vec<_>) = fields.unzip();
+            let (types, is_optionals): (Vec<_>, Vec<_>) = type_info.into_iter().unzip();
+            (field_names, types, is_optionals)
         }
         syn::Data::Enum(_) => todo!(),
         syn::Data::Union(_) => todo!(),
     };
 
+    let mandatory_fields = fields
+        .iter()
+        .zip(is_optionals.iter())
+        .filter(|(_, is_optional)| !**is_optional)
+        .map(|(field, _)| field);
+
+    let optional_fields = fields
+        .iter()
+        .zip(is_optionals.iter())
+        .filter(|(_, is_optional)| **is_optional)
+        .map(|(field, _)| field);
+
     // `CommandBuilder` is the name of the builder struct.
     let builder_name = format_ident!("{}Builder", name);
+
+    // impl Command
+    let impl_command = quote! {
+        impl #name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#fields: None,)*
+                }
+            }
+        }
+    };
+    // impl CommandBuilder
     let mut impl_builder = quote! {
         pub struct #builder_name {
             #( #fields: Option<#types>, )*
-            #( #optional_fields: Option<#optional_types>, )*
         }
         impl #builder_name {
             #(fn #fields(&mut self, #fields: #types) -> &mut Self {
@@ -97,34 +95,18 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 self
             })*
 
-            #(fn #optional_fields(&mut self, #optional_fields: #optional_types) -> &mut Self {
-                self.#optional_fields = Some(#optional_fields);
-                self
-            })*
-
             pub fn build(&mut self) -> Result<Command, Box<dyn std::error::Error>> {
                 Ok(Command {
-                    #(#fields: self.#fields.to_owned().ok_or(
-                        format!("missing field `{}`", stringify!(#fields))
+                    #(#mandatory_fields: self.#mandatory_fields.to_owned().ok_or(
+                        format!("missing field `{}`", stringify!(#mandatory_fields))
                     )?,)*
                     #(#optional_fields: self.#optional_fields.to_owned(),)*
                 })
             }
         }
     };
-    // `Command` is the name of the struct we are deriving for.
-    let impl_command = quote! {
 
-        impl #name {
-            pub fn builder() -> #builder_name {
-                #builder_name {
-                    #(#fields: None,)*
-                    #(#optional_fields: None,)*
-                }
-            }
-        }
-    };
-
+    // combine the two impls tokenstreams
     impl_builder.extend(impl_command);
     impl_builder.into()
 }
